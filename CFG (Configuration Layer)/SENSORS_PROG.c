1ed2ff5e07c07d16181a5b70c48915936fe95eb9
mohammed-darrag <mohammeddarrag13@gmail.com>
2024-12-17 01:30:06 +0100

    Configuration

---
+++ CFG (Configuration Layer)/SENSORS_PROG.c
@@ -0,0 +1,16 @@
+
+#include "STDtypes.h"
+#include "ADC_INTER.h"
+#include "SENSORS_INTER.h"
+#include "SENSORS_CFG.h"
+
+
+u16 sensors_readtemp(void)
+{
+	u16 volt =ADC_Readvoit(LM35_CH);
+	u16 temp=volt;
+	return temp ;
+	
+}
+
+
